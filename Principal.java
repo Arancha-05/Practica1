78af0133258706d96958a33e6582fe00482406d0
daw23-03 <daw23-03@c16x08.iescierva.net>
2024-01-31 08:40:18 +0100

    P1 Añadir fecha y hora

--- Principal.java
+++ Principal.java
@@ -1,5 +1,13 @@
+import java.time.LocalDateTime;
 public class Principal{
     public static void main (String [] args){
         System.out.println("Hola mundo");
+        fecha();
+    }
+
+    public static void fecha(){
+    LocalDateTime date = LocalDateTime.now();
+    System.out.println(date.getDayOfMonth() + "/" + date.getMonth() + "/" + date.getYear() + " " + date.getHour()+":"+date.getMinute());
+
     }
 }
